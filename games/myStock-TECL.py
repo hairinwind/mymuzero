f6c1e1b32aaabe6e0b6b8a81eec361d6793bf867
yao <dong_yao@hotmail.com>
2022-01-11 15:25:55 -0800

    use connect4 or cartpole config by GameConfig

--- games/myStock-TECL.py
+++ games/myStock-TECL.py
@@ -8,6 +8,11 @@ import torch
 from .abstract_game import AbstractGame
 from myEnv.TECLStockEnv import TECLCustomEnv
 from myEnv.stockDataAlphaReader import countPerDay, symbolCount, signalCount
+from myEnv.gameConfig import GameConfig
+
+from games.connect4 import MuZeroConfig as Connect4Config
+from games.cartpole import MuZeroConfig as CartpoleConfig
+
 
 # from rich.console import Console
 
@@ -15,10 +20,14 @@ from myEnv.stockDataAlphaReader import countPerDay, symbolCount, signalCount
 
 class MuZeroConfig:
     def __init__(self):
+        self.gameConfig = GameConfig()
+        config = self.getNeuralNetworkConfig(self.gameConfig.neural_network_config)   
+        self.config = config 
+
         # More information is available here: https://github.com/werner-duvaud/muzero-general/wiki/Hyperparameter-Optimization
 
-        self.seed = 0  # Seed for numpy, torch and the game
-        self.max_num_gpus = 1  # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
+        self.seed = config.seed  # Seed for numpy, torch and the game
+        self.max_num_gpus = config.max_num_gpus  # Fix the maximum number of GPUs to use. It's usually faster to use a single GPU (set it to 1) if it has enough memory. None will use every GPUs available
 
 
         ### Game
@@ -34,102 +43,97 @@ class MuZeroConfig:
 
 
         ### Self-Play
-        self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
-        self.selfplay_on_gpu = False
-        self.max_moves = 500  # Maximum number of moves if game is not finished before
-        self.num_simulations = 10  # Number of future moves self-simulated
+        self.num_workers = config.num_workers  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.selfplay_on_gpu = config.selfplay_on_gpu
+        self.max_moves = self.gameConfig.max_moves  # Maximum number of moves if game is not finished before
+        self.num_simulations = self.gameConfig.num_simulations  # Number of future moves self-simulated
         self.discount = 0.997  # Chronological discount of the reward
-        self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
+        self.temperature_threshold = config.temperature_threshold  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
 
         # Root prior exploration noise
-        self.root_dirichlet_alpha = 0.25
-        self.root_exploration_fraction = 0.25
+        self.root_dirichlet_alpha = config.root_dirichlet_alpha
+        self.root_exploration_fraction = config.root_exploration_fraction
 
         # UCB formula
-        self.pb_c_base = 19652
-        self.pb_c_init = 1.25
+        self.pb_c_base = config.pb_c_base
+        self.pb_c_init = config.pb_c_init
 
 
 
         ### Network
-        self.network = "fullyconnected"  # "resnet" / "fullyconnected"
-        self.support_size = 10  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward)))
+        self.network = config.network  # "resnet" / "fullyconnected"
+        self.support_size = config.support_size  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward))) TODO
         
         # Residual Network
-        self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
-        self.blocks = 1  # Number of blocks in the ResNet
-        self.channels = 2  # Number of channels in the ResNet
-        self.reduced_channels_reward = 2  # Number of channels in reward head
-        self.reduced_channels_value = 2  # Number of channels in value head
-        self.reduced_channels_policy = 2  # Number of channels in policy head
-        self.resnet_fc_reward_layers = []  # Define the hidden layers in the reward head of the dynamic network
-        self.resnet_fc_value_layers = []  # Define the hidden layers in the value head of the prediction network
-        self.resnet_fc_policy_layers = []  # Define the hidden layers in the policy head of the prediction network
+        self.downsample = config.downsample  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
+        self.blocks = config.blocks  # Number of blocks in the ResNet
+        self.channels = config.channels  # Number of channels in the ResNet
+        self.reduced_channels_reward = config.reduced_channels_reward  # Number of channels in reward head
+        self.reduced_channels_value = config.reduced_channels_value  # Number of channels in value head
+        self.reduced_channels_policy = config.reduced_channels_policy  # Number of channels in policy head
+        self.resnet_fc_reward_layers = config.resnet_fc_reward_layers  # Define the hidden layers in the reward head of the dynamic network
+        self.resnet_fc_value_layers = config.resnet_fc_value_layers  # Define the hidden layers in the value head of the prediction network
+        self.resnet_fc_policy_layers = config.resnet_fc_policy_layers  # Define the hidden layers in the policy head of the prediction network
 
         # Fully Connected Network
-        self.encoding_size = 8
-        self.fc_representation_layers = []  # Define the hidden layers in the representation network
-        self.fc_dynamics_layers = [16]  # Define the hidden layers in the dynamics network
-        self.fc_reward_layers = [16]  # Define the hidden layers in the reward network
-        self.fc_value_layers = [16]  # Define the hidden layers in the value network
-        self.fc_policy_layers = [16]  # Define the hidden layers in the policy network
+        self.encoding_size = config.encoding_size
+        self.fc_representation_layers = config.fc_representation_layers  # Define the hidden layers in the representation network
+        self.fc_dynamics_layers = config.fc_dynamics_layers  # Define the hidden layers in the dynamics network
+        self.fc_reward_layers = config.fc_reward_layers  # Define the hidden layers in the reward network
+        self.fc_value_layers = config.fc_value_layers  # Define the hidden layers in the value network
+        self.fc_policy_layers = config.fc_policy_layers  # Define the hidden layers in the policy network
 
 
 
         ### Training
         self.results_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../results", os.path.basename(__file__)[:-3], datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S"))  # Path to store the model weights and TensorBoard logs
         self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
-        self.training_steps = 10000  # Total number of training steps (ie weights update according to a batch)
-        self.batch_size = 8  # Number of parts of games to train on at each training step
+        self.training_steps = self.gameConfig.training_steps  # Total number of training steps (ie weights update according to a batch)
+        self.batch_size = self.gameConfig.batch_size  # Number of parts of games to train on at each training step
         self.checkpoint_interval = 10  # Number of training steps before using the model for self-playing
         self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.train_on_gpu = torch.cuda.is_available()  # Train on GPU if available
 
-        self.optimizer = "Adam"  # "Adam" or "SGD". Paper uses SGD
-        self.weight_decay = 1e-4  # L2 weights regularization
-        self.momentum = 0.9  # Used only if optimizer is SGD
+        self.optimizer = config.optimizer  # "Adam" or "SGD". Paper uses SGD
+        self.weight_decay = config.weight_decay  # L2 weights regularization
+        self.momentum = config.momentum  # Used only if optimizer is SGD
 
         # Exponential learning rate schedule
-        self.lr_init = 0.005  # Initial learning rate
-        self.lr_decay_rate = 0.9  # Set it to 1 to use a constant learning rate
-        self.lr_decay_steps = 1000
+        self.lr_init = config.lr_init  # Initial learning rate
+        self.lr_decay_rate = config.lr_decay_rate  # Set it to 1 to use a constant learning rate
+        self.lr_decay_steps = config.lr_decay_steps
 
 
 
         ### Replay Buffer
-        self.replay_buffer_size = 300  # Number of self-play games to keep in the replay buffer ## TODO increasing this will increase the replay file size?
-        self.num_unroll_steps = 10  # Number of game moves to keep for every batch element
-        self.td_steps = 10  # Number of steps in the future to take into account for calculating the target value
-        self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
-        self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
+        self.replay_buffer_size = config.replay_buffer_size  # Number of self-play games to keep in the replay buffer ## TODO increasing this will increase the replay file size?
+        self.num_unroll_steps = self.gameConfig.num_unroll_steps  # Number of game moves to keep for every batch element
+        self.td_steps = self.gameConfig.td_steps  # Number of steps in the future to take into account for calculating the target value
+        self.PER = config.PER  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
+        self.PER_alpha = config.PER_alpha  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
 
         # Reanalyze (See paper appendix Reanalyse)
-        self.use_last_model_value = True  # Use the last model to provide a fresher, stable n-step value (See paper appendix Reanalyze)
-        self.reanalyse_on_gpu = False
+        self.use_last_model_value = config.use_last_model_value # Use the last model to provide a fresher, stable n-step value (See paper appendix Reanalyze)
+        self.reanalyse_on_gpu = config.reanalyse_on_gpu
 
 
 
         ### Adjust the self play / training ratio to avoid over/underfitting
-        self.self_play_delay = 0  # Number of seconds to wait after each played game
-        self.training_delay = 0  # Number of seconds to wait after each training step
-        self.ratio = 1.5  # Desired training steps per self played step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
+        self.self_play_delay = config.self_play_delay  # Number of seconds to wait after each played game
+        self.training_delay = config.training_delay  # Number of seconds to wait after each training step
+        self.ratio = config.ratio  # Desired training steps per self played step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
 
 
     def visit_softmax_temperature_fn(self, trained_steps):
-        """
-        Parameter to alter the visit count distribution to ensure that the action selection becomes greedier as training progresses.
-        The smaller it is, the more likely the best action (ie with the highest visit count) is chosen.
+        return self.config.visit_softmax_temperature_fn(trained_steps)
 
-        Returns:
-            Positive float.
-        """
-        if trained_steps < 0.5 * self.training_steps:
-            return 1.0
-        elif trained_steps < 0.75 * self.training_steps:
-            return 0.5
-        else:
-            return 0.25
-
+    def getNeuralNetworkConfig(self, neural_network_config):
+        if neural_network_config == 'cartpole':
+            print("use cartpole config...")
+            return CartpoleConfig()
+        if neural_network_config == 'connect4':
+            print("use connect4 config...")
+            return Connect4Config()
 class Game(AbstractGame):
     """
     Game wrapper.
